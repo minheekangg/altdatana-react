46feef3980e7da6d6f1cbdca067614d53df5e842
minheekangg <minheekangg@gmail.com>
2020-01-25 15:37:32 -0500

    make primary button reusable

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 
 export default class Welcome extends React.Component {
     render() {
         return (
             <div class="container" id="welcome" >
-                <section class="flex-box two-items header">
+                <section class="flex-box two-items header introduction">
                     <div class="item left">
                         <h2>Short Term Loans Reinvented</h2>
                         <p>We understand.</p>
                         <p>Everyone has emergencies</p>
+                        <Button className="primary-button">Get a quote</Button>
                     </div>
                     <div class="item right">
 
