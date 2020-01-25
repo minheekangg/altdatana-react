e0a825df90496ff747803e6c799122690ec12b5d
minheekangg <minheekangg@gmail.com>
2020-01-25 16:16:07 -0500

    add scrollmagic

--- src/components/welcome/introduction.js
+++ src/components/welcome/introduction.js
@@ -1,23 +1,22 @@
 import React from 'react';
+
 import { Button } from 'semantic-ui-react';
 
-export default class Welcome extends React.Component {
+export default class WelcomeIntroduction extends React.Component {
     render() {
         return (
-            <div class="container" id="welcome" >
                 <section class="flex-box two-items header introduction">
                     <div class="item left">
                         <h2>Short Term Loans Reinvented</h2>
                         <p>We understand.</p>
                         <p>Everyone has emergencies</p>
-                        <Button className="primary-button">Get a quote</Button>
+                        <Button className="primary-button">Apply Now</Button>
                     </div>
                     <div class="item right">
 
                     </div>
                 </section>
-
-            </div>
         )
     }
 }
+
