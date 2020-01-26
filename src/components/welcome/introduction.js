717da032bf264b44a6277e4d87bc7225c26d43c1
minheekangg <minheekangg@gmail.com>
2020-01-25 20:36:00 -0500

    first section responsive

--- src/components/welcome/introduction.js
+++ src/components/welcome/introduction.js
@@ -6,6 +6,10 @@ export default class WelcomeIntroduction extends React.Component {
     render() {
         return (
             <section class="flex-box two-items header introduction container">
+                <div class="item right">
+                    <img src={tempimg} alt="temporary img" />
+                    {/* TODO: replace image later */}
+                </div>
                 <div class="item left">
                     <h2>
                         <span style={{ padding: 0 }}>Short Term Loans</span>
@@ -14,10 +18,6 @@ export default class WelcomeIntroduction extends React.Component {
                     <p>We understand.</p>
                     <p>Everyone has emergencies</p>
                     <Button className="primary-button">Apply Now</Button>
-                </div>
-                <div class="item right">
-                    <img src={tempimg} alt="temporary img" />
-                    {/* TODO: replace image later */}
                 </div>
             </section>
         );
