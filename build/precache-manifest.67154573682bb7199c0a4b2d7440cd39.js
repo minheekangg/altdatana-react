23e733146d7ebc15de6f1e7627f83f87645d1183
minheekangg <minheekangg@gmail.com>
2020-01-26 01:05:10 -0500

    deployed

---
+++ build/precache-manifest.67154573682bb7199c0a4b2d7440cd39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "513c1c3c3ce40163afe4",
+    "url": "/altdatana/static/css/main.792a73a8.chunk.css"
+  },
+  {
+    "revision": "513c1c3c3ce40163afe4",
+    "url": "/altdatana/static/js/main.513c1c3c.chunk.js"
+  },
+  {
+    "revision": "e1420df167e976f9dd37",
+    "url": "/altdatana/static/js/runtime~main.e1420df1.js"
+  },
+  {
+    "revision": "0a305a216c3eaa464eda",
+    "url": "/altdatana/static/js/2.0a305a21.chunk.js"
+  },
+  {
+    "revision": "f29073747f702b601cc852563e83daa2",
+    "url": "/altdatana/static/media/temp.f2907374.png"
+  },
+  {
+    "revision": "93dccc4f43db5690e71392279eb4f17a",
+    "url": "/altdatana/index.html"
+  }
+];
