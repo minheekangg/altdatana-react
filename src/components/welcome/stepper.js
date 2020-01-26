a522927002ed08fd395a215122d4d3313d7d44b3
minheekangg <minheekangg@gmail.com>
2020-01-26 00:12:49 -0500

    stepper done responsive

--- src/components/welcome/stepper.js
+++ src/components/welcome/stepper.js
@@ -1,27 +1,57 @@
 import React from 'react';
 
-import dollarPicture from '../../images/dollar.png';
+import dollarPicture from '../../images/lock.png';
 
 export default class Stepper extends React.Component {
     render() {
         return (
-            <section className="stepper">
+            <section id="stepper">
                 <div className="container">
+                    <h2>Why we're better</h2>
                     <div className="flex-box four-items">
                         <div className="step item">
                             <div className="stepper-img">
-                                <img src={dollarPicture} alt="item1 temp"/>
+                                <img src={dollarPicture} alt="item1 temp" />
                             </div>
-                            hi
+
+                            <h5>Low rates</h5>
+                            <p>
+                                Machine-learning underwriting allows us to cap
+                                our maximum rate at [48]% APR.
+                            </p>
                         </div>
                         <div className="step item">
-                            second
+                            <div className="stepper-img">
+                                <img src={dollarPicture} alt="item1 temp" />
+                            </div>
+
+                            <h5>Low rates</h5>
+                            <p>
+                                Machine-learning underwriting allows us to cap
+                                our maximum rate at [48]% APR.
+                            </p>
                         </div>
                         <div className="step item">
-                            hi
+                            <div className="stepper-img">
+                                <img src={dollarPicture} alt="item1 temp" />
+                            </div>
+
+                            <h5>Low rates</h5>
+                            <p>
+                                Machine-learning underwriting allows us to cap
+                                our maximum rate at [48]% APR.
+                            </p>
                         </div>
                         <div className="step item">
-                           fourth
+                            <div className="stepper-img">
+                                <img src={dollarPicture} alt="item1 temp" />
+                            </div>
+
+                            <h5>Low rates</h5>
+                            <p>
+                                Machine-learning underwriting allows us to cap
+                                our maximum rate at [48]% APR.
+                            </p>
                         </div>
                     </div>
                 </div>
