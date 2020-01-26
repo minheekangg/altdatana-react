0857fe044dee94a23e552c53a5c43670e96f7da4
minheekangg <minheekangg@gmail.com>
2020-01-25 23:10:57 -0500

    fix flexbox issues

--- src/components/welcome/introduction.js
+++ src/components/welcome/introduction.js
@@ -5,19 +5,21 @@ import { Button } from 'semantic-ui-react';
 export default class Intro extends React.Component {
     render() {
         return (
-            <section class="flex-box two-items header introduction container">
-                <div class="item right">
-                    <img src={tempimg} alt="temporary img" />
-                    {/* TODO: replace image later */}
-                </div>
-                <div class="item left">
-                    <h2>
-                        <div style={{ padding: 0 }}>Short Term Loans</div>
-                        <div style={{ padding: 0 }}>Reinvented</div>
-                    </h2>
-                    <p>We understand.</p>
-                    <p>Everyone has emergencies</p>
-                    <Button className="primary-button bigger">Apply Now</Button>
+            <section className="introduction">
+                <div className="flex-box two-items container">
+                    <div class="item right">
+                        <img src={tempimg} alt="temporary img" />
+                        {/* TODO: replace image later */}
+                    </div>
+                    <div class="item left">
+                        <h2>
+                            <div style={{ padding: 0 }}>Short Term Loans</div>
+                            <div style={{ padding: 0 }}>Reinvented</div>
+                        </h2>
+                        <p>We understand.</p>
+                        <p>Everyone has emergencies</p>
+                        <Button className="primary-button bigger">Apply Now</Button>
+                    </div>
                 </div>
             </section>
         );
