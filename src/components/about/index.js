e8cacba431583fafd09ea90f639a33fdf3718414
minheekangg <minheekangg@gmail.com>
2020-02-08 15:57:02 -0500

    responsive first section

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -2,21 +2,19 @@ import React from 'react';
 
 const About = () => (
     <div id="about" className="container tight">
-        <section className="introduction">
+        <section id="mission">
             <p className="section-tag">Our mission</p >
             <h2>Challenging <br/> Consumer Finance</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </section>
+        <section >
             <div className="flex-box two-items container">
                 <div className="item right">
-
+                    <h3>
+                        Bringing Ethical lending back
+                    </h3>
                 </div>
                 <div className="item left">
-                    <h2>
-                        <div style={{ padding: 0 }}>Short Term Loans</div>
-                        <div style={{ padding: 0 }}>Reinvented</div>
-                    </h2>
-                    <p>We understand.</p>
-                    <p>Everyone has emergencies</p>
                 </div>
             </div>
         </section>
