95e98f228a7dd22ab82e2feda395ed66aa35609b
minheekangg <minheekangg@gmail.com>
2020-02-08 21:21:09 -0500

    start products page

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -4,14 +4,15 @@ import React from 'react';
 
 const Products = () => (
     <div id="products">
-        {/* <div className="container tighter">
+        <div className="container tight">
             <section>
-                <h2 className="white">Products</h2>
+                <p className="section-tag white">Products</p>
+                <h2 className="white">I am looking for...</h2>
                 <h3>product 1</h3>
                 <h3>product 2</h3>
                 <h3>product 3</h3>
             </section>
-        </div> */}
+        </div>
 
     </div>
 );
