47ab146b98023a762f9c5e69417b12eb246905e0
minheekangg <minheekangg@gmail.com>
2020-02-20 19:52:38 -0500

    curvy top bottom purple

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -6,49 +6,74 @@ import TempImg2 from '../../images/temptyg6.jpg';
 import { ROUTE_ROOT } from "../../utils/constants";
 
 const Products = () => (
-    <div id="products">
-        <div className="container tight">
-            <section>
-                <p className="section-tag">Products</p>
-                <h2 style={{ color: "black" }}>I am looking to finance...</h2>
-                <div className="products-list flex-box four-items">
-                    <a className="product item" href={`${ROUTE_ROOT}/products/1`}>
-                        <img src={TempImg} alt="school" />
-                        <div className="product-title">Loan for School</div>
-                        <p>
-                            Our home equity line allows you to consolidate debt
-                            at a lower rate compared to credit cards and
-                            personal loans.
-                        </p>
+    <div id="products" className="gradient">
+        <div className="curvy-border">
+            <div className="container tight">
+                <section>
+                    <p className="section-tag">Products</p>
+                    <h2 style={{ color: "black" }}>I am looking to finance...</h2>
+                    <div className="products-list flex-box four-items">
+                        <a className="product item" href={`${ROUTE_ROOT}/products/1`}>
+                            <img src={TempImg} alt="school" />
+                            <div className="product-title">Loan for School</div>
+                            <p>
+                                Our home equity line allows you to consolidate debt
+                                at a lower rate compared to credit cards and
+                                personal loans.
+                            </p>
 
-                        <div className="learn-more">+ Learn more</div>
-                    </a>
-                    <div className="product item">
-                        <img src={TempImg} alt="school" />
-                        <div className="product-title">Refinance</div>
-                        <p>
+                            <div className="learn-more">+ Learn more</div>
+                        </a>
+                        <div className="product item">
+                            <img src={TempImg} alt="school" />
+                            <div className="product-title">Refinance</div>
+                            <p>
+                                Our home equity line allows you to consolidate debt
+                                at a lower rate compared to credit cards and
+                                personal loans.
+                            </p>
+                        </div>
+                        <div className="product item">
+                            <img src={TempImg} alt="school" />
+                            <div className="product-title">Home</div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+        <div >
+            <div className="container tight">
+                <section class="product-deeper">
+
+                </section>
+                <section class="product-deeper">
+
+                        <div class="img-wrapper">
+                            <img src={TempImg2} alt='phone in hand' />
+                            <p class="img-tag">Photo by Balázs Kétyi on Unsplash</p>
+                        </div>
+                        <div class="desc">
                             Our home equity line allows you to consolidate debt
-                            at a lower rate compared to credit cards and
-                            personal loans.
-                        </p>
-                    </div>
-                    <div className="product item">
-                        <img src={TempImg} alt="school" />
-                        <div className="product-title">Home</div>
-                    </div>
-                </div>
-            </section>
-            <section class="product-deeper">
-                <div class="img-wrapper">
-                    <img src={TempImg2} alt='phone in hand' />
-                    <p class="img-tag">Photo by Balázs Kétyi on Unsplash</p>
-                </div>
-                <div class="desc">
-                    Our home equity line allows you to consolidate debt
-                            at a lower rate compared to credit cards and
-                            personal loans.
-                </div>
-            </section>
+                                    at a lower rate compared to credit cards and
+                                    personal loans.
+                        </div>
+
+                </section>
+      
+                <section class="product-deeper">
+
+                        <div class="img-wrapper">
+                            <img src={TempImg2} alt='phone in hand' />
+                            <p class="img-tag">Photo by Balázs Kétyi on Unsplash</p>
+                        </div>
+                        <div class="desc">
+                            Our home equity line allows you to consolidate debt
+                                    at a lower rate compared to credit cards and
+                                    personal loans.
+                        </div>
+
+                </section>
+            </div>
         </div>
         <CallToAction type="apply" />
     </div>
