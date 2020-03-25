f5fbf227c540a6fcc4ff3007197e5dc2a9f7c97b
minheekangg <minheekangg@gmail.com>
2020-03-25 16:10:19 -0400

    currency formatter

--- src/components/dashboard/dashboard-main/data/list.js
+++ src/components/dashboard/dashboard-main/data/list.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { List } from "semantic-ui-react";
+import CurrencyFormat from "react-currency-format";
 
 import { CATEGORIES_ICON, CATEGORIES } from "../variables";
 
@@ -13,22 +14,25 @@ function ListWrapper(props) {
             {propsArray(props.data).map(c=> {
                 const category = CATEGORIES[c];
                  return (
-                    <List.Item key={category}>
-                        <List.Icon
-                            name={CATEGORIES_ICON[category]}
-                            size="large"
-                            verticalAlign="middle"
-                        />
-                        <List.Content>
-                            <List.Header as="a">
-                                {c}
-                            </List.Header>
-                            <List.Description as="a">
-                                {props.data[c]}
-                            </List.Description>
-                        </List.Content>
-                    </List.Item>
-                    )
+                     <List.Item key={category}>
+                         <List.Icon
+                             name={CATEGORIES_ICON[category]}
+                             size="large"
+                             verticalAlign="middle"
+                         />
+                         <List.Content style={{textAlign: 'right'}}>
+                             <List.Header as="a">{c}</List.Header>
+                             <List.Description as="a">
+                                 <CurrencyFormat
+                                     value={props.data[c]}
+                                     displayType={"text"}
+                                     thousandSeparator={true}
+                                     prefix={"$"}
+                                 />
+                             </List.Description>
+                         </List.Content>
+                     </List.Item>
+                 );
                 })
             }
         </List>
