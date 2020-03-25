e2f30785962a79531710cfd436a25976e0649436
minheekangg <minheekangg@gmail.com>
2020-03-25 18:57:12 -0400

    adjusted border box

--- src/components/dashboard/dashboard-main/index.js
+++ src/components/dashboard/dashboard-main/index.js
@@ -16,6 +16,9 @@ export default class DashboardMain extends React.Component {
     render(){
         return (
             <div className="dashboard-main container">
+                <div className="recap-banner">
+                    During this month you spent <strong>$1000</strong>, which is <span>+15%</span> from last year.
+                </div>
                 <div className="box-fourty">
                     <div className="data-container stat-card">
                         <h3>Largest Spending</h3>
@@ -29,6 +32,12 @@ export default class DashboardMain extends React.Component {
                         <p>$12,158</p>
                         <Progress percent={10} progress />
                     </div>
+                    <div className="data-container stat-card">
+                        <h3>One more Spending</h3>
+                        <p>Sporting Goods</p>
+                        <p>$12,158</p>
+                        <Progress percent={99} progress />
+                    </div>
                 </div>
                 <div className="data-container box-sixty">
                     <h3>Total Spending</h3>
@@ -39,8 +48,7 @@ export default class DashboardMain extends React.Component {
                 <div className="data-container">
                     <h3>Total Spending</h3>
                     <p>description of categories</p>
-                    <CategoriesDonut data={altData} />
-                    <List data={altData} />
+                   
                 </div>
             </div>
         );
