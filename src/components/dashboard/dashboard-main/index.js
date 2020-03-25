d99c4dc4b87e2825e9798731d52a76615fb3299a
minheekangg <minheekangg@gmail.com>
2020-03-25 18:11:14 -0400

    finished stat

--- src/components/dashboard/dashboard-main/index.js
+++ src/components/dashboard/dashboard-main/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {response} from './response';
+import { Progress } from "semantic-ui-react";
 
+import { response, altData } from "./response";
 import CategoriesDonut from "./data/categories-donut";
 import List from './data/list';
-import { altData } from './response';
 
 export default class DashboardMain extends React.Component {
     constructor(props) {
@@ -17,19 +17,17 @@ export default class DashboardMain extends React.Component {
         return (
             <div className="dashboard-main container">
                 <div className="box-fourty">
-                    <div className="data-container">
+                    <div className="data-container stat-card">
                         <h3>Largest Spending</h3>
                         <p>Restaurants</p>
-                        <p>$</p>
+                        <p>$51,338</p>
+                        <Progress percent={44} progress />
                     </div>
-                    <div className="data-container">
-                        <h3>Smallest Expense</h3>
+                    <div className="data-container stat-card">
+                        <h3>Smallest Spending</h3>
                         <p>Sporting Goods</p>
-                        <p>$</p>
-                    </div>
-                    <div className="data-container">
-                        <h3>Largest Spending</h3>
-                        <p>Restaurants</p>
+                        <p>$12,158</p>
+                        <Progress percent={10} progress />
                     </div>
                 </div>
                 <div className="data-container box-sixty">
