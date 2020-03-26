b355548dbc25cbcf479d8653dd065ed67ede339c
minheekangg <minheekangg@gmail.com>
2020-03-26 12:44:34 -0400

    added barchart

--- src/components/dashboard/dashboard-main/data/barchart.js
+++ src/components/dashboard/dashboard-main/data/barchart.js
@@ -42,13 +42,13 @@ const data = [
 function BarChartWrapper(props) {
     return (
         < BarChart width = { 730} height = { 250} data = { data } >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="pv" fill="#8884d8" />
-                <Bar dataKey="uv" fill="#82ca9d" />
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="pv" fill="#8884d8" />
+            <Bar dataKey="uv" fill="#82ca9d" />
         </BarChart >
     )
 }
--- src/components/dashboard/dashboard-main/data/categories-donut.js
+++ src/components/dashboard/dashboard-main/data/categories-donut.js
@@ -13,11 +13,9 @@ const data = [
 */
 
 const renderActiveShape = props => {
-    const RADIAN = Math.PI / 180;
     const {
         cx,
         cy,
-        midAngle,
         innerRadius,
         outerRadius,
         startAngle,
@@ -27,15 +25,6 @@ const renderActiveShape = props => {
         percent,
         value
     } = props;
-    const sin = Math.sin(-RADIAN * midAngle);
-    const cos = Math.cos(-RADIAN * midAngle);
-    const sx = cx + (outerRadius + 10) * cos;
-    const sy = cy + (outerRadius + 10) * sin;
-    const mx = cx + (outerRadius + 30) * cos;
-    const my = cy + (outerRadius + 30) * sin;
-    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-    const ey = my;
-    const textAnchor = cos >= 0 ? "start" : "end";
 
     return (
         <g>
@@ -66,27 +55,6 @@ const renderActiveShape = props => {
                 outerRadius={outerRadius + 10}
                 fill={fill}
             />
-            {/* <path
-                d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
-                stroke={fill}
-                fill="none"
-            />
-            <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-            <text
-                x={ex + (cos >= 0 ? 1 : -1) * 12}
-                y={ey}
-                textAnchor={textAnchor}
-                fill="#333"
-            >{`PV ${value}`}</text>
-            <text
-                x={ex + (cos >= 0 ? 1 : -1) * 12}
-                y={ey}
-                dy={18}
-                textAnchor={textAnchor}
-                fill="#999"
-            >
-                {`(Rate ${(percent * 100).toFixed(2)}%)`}
-            </text> */}
         </g>
     );
 };
