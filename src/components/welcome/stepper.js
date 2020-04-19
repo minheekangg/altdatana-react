c2f81baf82bf4088b47fa2a600031e85366532e7
minheekangg <minheekangg@gmail.com>
2020-04-18 22:17:28 -0400

    added copy for stepper

--- src/components/welcome/stepper.js
+++ src/components/welcome/stepper.js
@@ -7,20 +7,40 @@ export default class Stepper extends React.Component {
         return (
             <section id="stepper" className="bg-grey">
                 <div className="container">
-                    <h2>Why we're better</h2>
+                    <h2>Smarter Decisions,</h2>
+                    <h2>Backed By Powerful Data</h2>
                     <div className="flex-box four-items">
                         <div className="step item">
                             <div className="stepper-img">
                                 <img src={dollarPicture} alt="item1 temp" />
                             </div>
 
-                            <h5>Low rates</h5>
+                            <h5>Easy Setup and Onboarding</h5>
                             <p>
-                                Machine-learning underwriting allows us to cap
-                                our maximum rate at [48]% APR.
+                                Whether you integrate our API into your business ecosystem, or use our interface to face your client, our onboarding team is here for you.
                             </p>
                         </div>
                         <div className="step item">
+                            <div className="stepper-img">
+                                <img src={dollarPicture} alt="item1 temp" />
+                            </div>
+
+                            <h5>Encryption and Security</h5>
+                            <p>
+                                We don’t keep the borrower’s data, and we encrypt everything to ensure only your team has access. 
+                            </p>
+                        </div>
+                        <div className="step item">
+                            <div className="stepper-img">
+                                <img src={dollarPicture} alt="item1 temp" />
+                            </div>
+
+                            <h5>Complete Customization</h5>
+                            <p>
+                                Create a credit application through us, or simply use our analyticals for your underwriting 
+                            </p>
+                        </div>
+                        {/* <div className="step item">
                             <div className="stepper-img">
                                 <img src={dollarPicture} alt="item1 temp" />
                             </div>
@@ -30,29 +50,7 @@ export default class Stepper extends React.Component {
                                 Machine-learning underwriting allows us to cap
                                 our maximum rate at [48]% APR.
                             </p>
-                        </div>
-                        <div className="step item">
-                            <div className="stepper-img">
-                                <img src={dollarPicture} alt="item1 temp" />
-                            </div>
-
-                            <h5>Low rates</h5>
-                            <p>
-                                Machine-learning underwriting allows us to cap
-                                our maximum rate at [48]% APR.
-                            </p>
-                        </div>
-                        <div className="step item">
-                            <div className="stepper-img">
-                                <img src={dollarPicture} alt="item1 temp" />
-                            </div>
-
-                            <h5>Low rates</h5>
-                            <p>
-                                Machine-learning underwriting allows us to cap
-                                our maximum rate at [48]% APR.
-                            </p>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </section>
