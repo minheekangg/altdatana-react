3c0bd83b7aae7b057d1101a5c9e4c1f24c2d2421
minheekangg <minheekangg@gmail.com>
2020-04-30 01:47:32 -0400

    done with nav

--- src/components/welcome/stepper.js
+++ src/components/welcome/stepper.js
@@ -1,18 +1,15 @@
 import React from 'react';
 
-// import dollarPicture from '../../images/lock.png';
-
 export default class Stepper extends React.Component {
     render() {
         return (
             <section id="stepper" className="bg-navy">
                 <div className="container">
-                    <h3>Smarter Decisions, Backed By Powerful Data</h3>
+                    <h3>Smarter Decisions, <br/>Backed By Powerful Data</h3>
                     <div className="flex-box four-items">
                         <div className="step item">
                             <div className="stepper-img">
                             </div>
-
                             <h5>Easy Setup and Onboarding</h5>
                             <p>
                                 Whether you integrate our API into your business ecosystem, or use our interface to face your client, our onboarding team is here for you.
@@ -21,7 +18,6 @@ export default class Stepper extends React.Component {
                         <div className="step item">
                             <div className="stepper-img">
                             </div>
-
                             <h5>Encryption and Security</h5>
                             <p>
                                 We don’t keep the borrower’s data, and we encrypt everything to ensure only your team has access. 
