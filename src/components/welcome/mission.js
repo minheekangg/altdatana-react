d297a4a7ff226f2775a03cfec9abf127f796b3ae
minheekangg <minheekangg@gmail.com>
2020-04-30 00:15:25 -0400

    starting moving over missions

--- src/components/welcome/mission.js
+++ src/components/welcome/mission.js
@@ -1,9 +1,66 @@
 import React from 'react';
 import { Icon } from 'semantic-ui-react';
-import tempimg from '../../images/shark.png';
+import styled from "styled-components";
 
-import { Controller, Scene } from "react-scrollmagic";
-import { Tween } from "react-gsap";
+const IntroMissionSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: black;
+
+    .mission {
+        height: 100vh;
+        width: 100%;
+        text-align: left;
+
+        h2 { font-size: 42px; }
+
+        .left {
+            align-self: center;
+            margin-bottom: 30px;
+        }
+
+    }
+`;
+
+const StyledList = styled.ul`
+    list-style: none;
+    position: relative;
+    text-align: left;
+    padding-left: 60px;
+
+    li {
+        cursor: pointer;
+    }
+
+    li:before {
+        content: "";
+        width: 30px;
+        height: 30px;
+        position: absolute;
+        background-image: url("../../images/list-icon.png");
+        background-size: cover;
+        background-position: center;
+        left: 0;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+
+    h6 {
+        margin-bottom: 10px;
+    }
+
+    li.open p {
+        opacity: 1;
+        height: auto;
+        pointer-events: all;
+    }
+    // li.closed p {
+    //     opacity: 0;
+    //     height: 0;
+    //     pointer-events: none;
+    // }
+`;
 
 export default class Mission extends React.Component {
     state = { openOne: false, openTwo: false, openThree : false }
@@ -11,108 +68,71 @@ export default class Mission extends React.Component {
     render() {
         const { openOne, openTwo, openThree } = this.state
         return (
-            <section className="mission" >
+            <IntroMissionSection class="mission">
                 <div className="flex-box container">
                     <div className="item left">
-                        <img src={tempimg} alt="temporary img" />
+                        <StyledList>
+                            <li
+                                onClick={() =>
+                                    this.setState({
+                                        openOne: !openOne,
+                                    })
+                                }
+                                className={openOne ? "open" : "closed"}
+                            >
+                                <h6>
+                                    Borrower Behavioral Analysis
+                                </h6>
+                                <p>
+                                    Analyze your borrower’s payment history to
+                                    predict their creditworthiness.
+                                </p>
+                            </li>
+                            <li
+                                onClick={() =>
+                                    this.setState({
+                                        openTwo: !openTwo,
+                                    })
+                                }
+                                className={openTwo ? "open" : "closed"}
+                            >
+                                <h6>
+                                    Seamless Integration
+                                </h6>
+                                <p>
+                                    We stay behind the scenes, so you can build
+                                    the client relationship directly
+                                </p>
+                            </li>
+                            <li
+                                onClick={() =>
+                                    this.setState({
+                                        openThree: !openThree,
+                                    })
+                                }
+                                className={openThree ? "open" : "closed"}
+                            >
+                                <h6>
+                                    Stochastic Simulations
+                                </h6>
+                                <p>
+                                    Model a borrower’s behavior against your
+                                    loan terms, giving you confidence in your
+                                    underwrite
+                                </p>
+                            </li>
+                        </StyledList>
                     </div>
                     <div className="item right">
-                            <div className="mission-header">
-                                <Controller>
-                                    <Scene triggerElement=".mission-header" duration={500} >
-                                        {progress => (
-                                            <Tween
-                                                from={{
-                                                    opacity: 0,
-                                                    yPercent: 10
-                                                }}
-                                                to={{
-                                                    opacity: 1
-                                                }}
-                                                ease="Back.easeOut"
-                                                totalProgress={progress}
-                                                paused
-                                            >
-                                                <h2 className="white">What we offer</h2>
-                                            </Tween>
-                                        )}
-                                    </Scene>
-                                    <Scene triggerElement=".mission" duration={300}>
-                                        {progress => (
-                                            <Tween
-                                                from={{
-                                                    scaleX: 0,
-                                                    transformOrigin: "left"
-                                                }}
-                                                to={{
-                                                    scaleX: 1
-                                                }}
-                                                ease="Linear.ease"
-                                                totalProgress={progress}
-                                                paused
-                                            >
-                                                <hr />
-                                            </Tween>
-                                        )}
-                                    </Scene>
-                                </Controller>
-                            </div>
-                            <Controller>
-                                <Scene triggerElement=".mission h2" duration={500}>
-                                    {progress => (
-                                        <Tween
-                                            from={{
-                                                opacity: 0,
-                                            }}
-                                            to={{
-                                                opacity: 1
-                                            }}
-                                            ease="Back.easeOut"
-                                            totalProgress={progress}
-                                            paused
-                                        >
-                                            <p>
-                                                Still underwriting your borrowers with W2s and paystubs? Modernize your business with advanced analytics on your client’s payment behavior. 
-                                            </p>
-                                            <ul>
-                                                <li onClick={() => this.setState({openOne: !openOne})} className={openOne ? 'open' : 'closed'}>
-                                                    <h3>
-                                                        <Icon name={openOne ? 'minus circle' : 'plus circle'} />
-                                                        Borrower Behavioral Analysis</h3>
-                                                    <p>
-                                                        Analyze your borrower’s payment history to predict their creditworthiness.
-                                                    </p>
-                                                </li>
-                                                <li onClick={() => this.setState({ openTwo: !openTwo })} className={openTwo ? 'open' : 'closed'}>
-                                                    
-                                                    <h3> 
-                                                        <Icon name={openTwo ? 'minus circle' : 'plus circle'}  />
-                                                        Seamless Integration 
-                                                        </h3>
-                                                    <p>
-                                                        We stay behind the scenes, so you can build the client relationship directly
-                                                    </p>
-                                                </li>
-                                                <li onClick={() => this.setState({ openThree: !openThree })} className={openThree ? 'open' : 'closed'}>
-                                                    
-                                                    <h3> 
-                                                        <Icon name={openThree ? 'minus circle' : 'plus circle'}  />
-                                                        Stochastic Simulations
-                                                        </h3>
-                                                    <p>
-                                                        Model a borrower’s behavior against your loan terms, giving you confidence in your underwrite
-                                                    </p>
-                                                </li>
-                                            </ul>
-                                            
-                                            
-                                        </Tween>
-                                    )}
-                                </Scene>
-                            </Controller>
-                        </div>
+                        <h3>What we offer</h3>
+                        <p>
+                            Still underwriting your borrowers with W2s and
+                            paystubs? Modernize your business with advanced
+                            analytics on your client’s payment behavior.
+                        </p>
                     </div>
-            </section>
+                </div>
+            </IntroMissionSection>
         );
     }
 }
