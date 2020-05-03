97ebec460fe81b212a8aa9aab1d937e8b648de0e
minheekangg <minheekangg@gmail.com>
2020-05-03 12:09:12 -0400

    about-page setup done

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,44 +1,87 @@
 import React from 'react';
+import styled from "styled-components";
 
 import CallToAction from '../call-to-action';
 
+import tempimg from '../../images/temp.png';
+
+const AboutSectionTitle = styled.section`
+    .flex-box { align-items: center; }
+    .item.right {
+        flex-basis: 50%;
+        img {
+            width: 100%;
+        }
+    }
+`;
+const AboutSectionMid = styled.section`
+    max-width: 700px;
+    margin: auto;
+
+    h3 { font-size: 48px;}
+`;
+const AboutSectionContent = styled.section`
+    text-align: left;
+    .flex-box {
+        justify-content: space-between;
+        align-items: center;
+    }
+    .item { 
+        flex-basis: 50%;
+        padding: 20px;
+    }
+    .content-header {
+        font-size: 30px;
+    }
+    img {
+        width: 100%;
+    }
+
+    &last-child {
+     margin-bottom: 100px;   
+    }
+`;
+
 const About = () => (
-    <div id="about" >
-        <div className="container tight">
-            <section id="mission">
-                <p className="section-tag">Our mission</p >
-                <h2>Challenging <br/> Consumer Finance</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </section>
-        </div>
-        <div className="bg-grey">
-            <div className="container tight">
-                <section className="about-list">
-                    <div className="flex-box two-items container">
-                        <div className="item right">
-                            <h3 >
-                                Word from Our Leadership
-                            </h3>
-                        </div>
-                        <div className="item left">
-                            <div className="mission-list">
-                                <div className="mission-card">
-                                    <p className="section-tag bold">Accessible Financing</p>
-                                    <p>We position ourselves as an alternative option to the payday or personal loan industry. Affluent provides simple financing options with quick access to cash.</p>
-                                </div>
-                                <div className="mission-card">
-                                    <p className="section-tag bold">Accessible Financing</p>
-                                    <p>We position ourselves as an alternative option to the payday or personal loan industry. Affluent provides simple financing options with quick access to cash.</p>
-                                </div>
-                                <div className="mission-card">
-                                    <p className="section-tag bold">Accessible Financing</p>
-                                    <p>We position ourselves as an alternative option to the payday or personal loan industry. Affluent provides simple financing options with quick access to cash.</p>
-                                </div>
-                            </div>
-                        </div>
+    <div id="about">
+        <div className="container">
+
+            <AboutSectionTitle>
+                <div className="flex-box two-items container">
+                    <div className="item left">
+                        <h2> About us</h2>
                     </div>
-                </section>
-            </div>
+                    <div className="item right">
+                        <img src={tempimg} alt="temporary img" />
+                    </div>
+                </div>
+            </AboutSectionTitle>
+            <AboutSectionMid>
+                <h3> We love data, and we think the world should too.</h3>
+            </AboutSectionMid>
+            <AboutSectionContent>
+                <div className="flex-box">
+                    <div className="item left">
+                        <h3 className="content-header">Our Goals</h3>
+                        <p>While large institutions spend millions of dollars on development and underwriting technology, small lenders and credit unions are left to continue using credit scores, W2s, and pay stubs, to understand a borrower’s financial risk.</p>
+                        <p>We are here to provide a solution to modernize underwriting in a big way, without needing to develop an R&D department, for every institution that faces lending risk.</p>
+                    </div>
+                    <div className="item right">
+                        <img src={tempimg} alt="temporary img" />
+                    </div>
+                </div>
+            </AboutSectionContent>
+            <AboutSectionContent>
+                <div className="flex-box">
+                    <div className="item right">
+                        <img src={tempimg} alt="temporary img" />
+                    </div>
+                    <div className="item left">
+                        <h3 className="content-header">Our Experience</h3>
+                        <p>We are a team made up of credit analysts, risk managers, actuaries, attorneys, data scientists, and investment bankers. We’ve been at the forefront of institutional risk management on all sides of the street. </p>
+                    </div>
+                </div>
+            </AboutSectionContent>
         </div>
         <CallToAction type="question"/>
     </div>
